64e37f21a07c664985aa8fef7fe7253d0eadbc8d
首藤優誠 <111882261+shutouyusei@users.noreply.github.com>
2024-11-11 18:00:48 +0900

    FirstCommit

---
+++ Source/MagicKnight/MagicKnightGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MagicKnightGameMode.h"
+#include "MagicKnightCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMagicKnightGameMode::AMagicKnightGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
